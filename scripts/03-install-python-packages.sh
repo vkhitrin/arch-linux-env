c0d44257de3e211ffe6954f98e20c498486988a3
Vadim Khitrin <me@vkhitrin.com>
2024-10-05 01:41:14 +0300

    chore: Update System|Python Packages
    Updating system packages.
    
    Fixing python packages script.

--- scripts/03-install-python-packages.sh
+++ scripts/03-install-python-packages.sh
@@ -6,10 +6,10 @@ source ./scripts/common.sh
 print_padded_title "Brew pipx - Install Additional Software"
 declare -a PIPX_PACKAGES
 declare -a PIPX_HARLEQUIN_PACKAGES
-PIPX_PACKAGES=("passhole" "harlequin" "scylla-cqlsh" "rexi")
+PIPX_PACKAGES=("harlequin" "cqlsh" "rexi")
 PIPX_HARLEQUIN_PACKAGES=("catppuccin[pygments]" "boto3" "harlequin-postgres" "harlequin-mysql" "harlequin-odbc" "harlequin-cassandra" "git+https://github.com/ThomAub/harlequin-clickhouse")
-${HOMEBREW_BIN_PATH_PREFIX}/pipx install "${PIPX_PACKAGES[@]}"
+pipx install "${PIPX_PACKAGES[@]}"
 for HARLEQUIN_PACKAGE in "${PIPX_HARLEQUIN_PACKAGES[@]}"; do
-    ${HOMEBREW_BIN_PATH_PREFIX}/pipx inject harlequin "${HARLEQUIN_PACKAGE}"
+    pipx inject harlequin "${HARLEQUIN_PACKAGE}"
 done
-${HOMEBREW_BIN_PATH_PREFIX}/pipx upgrade-all --include-injected
+pipx upgrade-all --include-injected
