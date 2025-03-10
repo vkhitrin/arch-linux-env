8f91776b07b772267f29936945fc1668df5af5ed
Vadim Khitrin <me@vkhitrin.com>
2025-03-10 23:51:08 +0200

    chore: Update Configuration
    * Updating `mackup`.
    * Updating packages.
    * Updating scripts.

--- scripts/03-install-python-packages.sh
+++ scripts/03-install-python-packages.sh
@@ -3,11 +3,11 @@ set -eo pipefail
 
 source ./scripts/common.sh
 
-print_padded_title "Brew pipx - Install Additional Software"
+print_padded_title "pipx - Install Additional Software"
 declare -a PIPX_PACKAGES
 declare -a PIPX_HARLEQUIN_PACKAGES
-PIPX_PACKAGES=("harlequin" "cqlsh" "rexi")
-PIPX_HARLEQUIN_PACKAGES=("catppuccin[pygments]" "boto3" "harlequin-postgres" "harlequin-mysql" "harlequin-odbc" "harlequin-cassandra" "git+https://github.com/ThomAub/harlequin-clickhouse")
+PIPX_PACKAGES=("harlequin" "cqlsh" "rexi" "passhole")
+PIPX_HARLEQUIN_PACKAGES=("boto3" "harlequin-postgres" "harlequin-mysql" "harlequin-odbc")
 pipx install "${PIPX_PACKAGES[@]}"
 for HARLEQUIN_PACKAGE in "${PIPX_HARLEQUIN_PACKAGES[@]}"; do
     pipx inject harlequin "${HARLEQUIN_PACKAGE}"
